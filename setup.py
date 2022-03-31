66aa4b44d0ac60a1e6334c5927cade91a54a34ff
abekor <102305217+abekor@users.noreply.github.com>
2022-03-31 14:23:11 -0230

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='practice_function',
+    version='0.0.1',
+    author='Mike Huls',
+    author_email='a.kor@metricsflow.com',
+    description='Testing installation of Package',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/abekor',
+    project_urls = {
+        "Bug Tracker": "https://github.com/abekor"
+    },
+    license='MIT',
+    packages=['practice_function'],
+    install_requires=['requests'],
+)
